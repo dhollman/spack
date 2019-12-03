02cf940728e55f758185f381817bb703c432cf0e
David S Hollman <dshollm@blake.sandia.gov>
2019-12-02 18:31:31 -0700

    changes to rust

--- var/spack/repos/builtin/packages/rust/package.py
+++ var/spack/repos/builtin/packages/rust/package.py
@@ -11,13 +11,17 @@ class Rust(Package):
 
     homepage = "http://www.rust-lang.org"
     git      = "https://github.com/rust-lang/rust.git"
+    url = "https://github.com/rust-lang/rust/archive/1.39.0.tar.gz"
 
     version('develop', branch='master')
+    version('1.39.0', md5='f3ca7fcd83e45d92d7cd8c93777b56f3')
+    
     version('1.34.0', tag='1.34.0')
     version('1.32.0', tag='1.32.0')
     version('1.31.1', tag='1.31.1')
     version('1.31.0', tag='1.31.0')  # "Rust 2018" edition
     version('1.30.1', tag='1.30.1')
+    
 
     extendable = True
 
@@ -28,11 +32,22 @@ class Rust(Package):
     depends_on("cmake")
     depends_on("binutils")
     depends_on("python@:2.8")
+    #depends_on("python", when="@1.35.0:")
 
     # Cargo
     depends_on("openssl")
 
     phases = ['configure', 'install']
+
+    def setup_build_environment(self, env):
+        # No idea why the python package doesn't do this
+        env.append_path("PYTHONPATH",
+            join_path(self.spec['python'].prefix, self.spec['python'].package.python_lib_dir))
+
+    def setup_run_environment(self, env):
+        # No idea why the python package doesn't do this
+        env.append_path("PYTHONPATH",
+            join_path(self.spec['python'].prefix, self.spec['python'].package.python_lib_dir))
 
     def configure(self, spec, prefix):
         configure_args = [
